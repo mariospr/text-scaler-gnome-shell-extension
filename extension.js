bca87a0c34a489b94c6e6515aee2af326d4a73e1
Mario Sanchez Prada <mario@mariospr.org>
2015-11-27 18:31:50 +0000

    Create a new button in the status area (zoom-in icon for now)

--- extension.js
+++ extension.js
@@ -1,13 +1,33 @@
+// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
+
 const Lang = imports.lang;
 const St = imports.gi.St;
 const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+let _button = null;
+
+const FontResizerButton = new Lang.Class({
+    Name: 'FontResizerButton',
+    Extends: PanelMenu.Button,
+
+    _init: function() {
+        this.parent(0.0, "Font Resizer Button");
+
+        this._icon = new St.Icon({ icon_name: 'zoom-in',
+                                   style_class: 'system-status-icon' });
+        this.actor.add_child(this._icon);
+    }
+});
 
 function init() {
 }
 
 function enable() {
-    Main.notify("Hello World!");
+    _button = new FontResizerButton();
+    Main.panel.addToStatusArea('font-resizer-button', _button);
 }
 
 function disable() {
+    _button.destroy();
 }
